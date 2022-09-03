f3d97d895e1b8daf3d61c540ad3f87b38b3c01c4
Arif Laksonodhewo <arifldhewo234@gmail.com>
2022-09-03 21:21:03 +0700

    Fitur Dashboard sudah ditambah card, tinggal insert gambar

--- dashboard.php
+++ dashboard.php
@@ -2,27 +2,79 @@
 
 <?php
 
-if (isset($_SESSION['fullnames'])) :
+    if (isset($_SESSION['fullnames'])) :
 
 ?>
 
-    
-    <?php include_once 'navbar.php'; ?>
+<?php include_once 'navbar.php'; ?>
 
     <div class="container">
+        <div class="d-flex justify-content-center">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/uploads/1203181.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/uploads/1203335.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/uploads/1209038.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+            </div>
+        </div>
 
+        <hr>
+        <?php
+
+        require 'inc/dashboard.inc.php';
+
+        ?>
+
+    <?php foreach($fetch_tb_lapak_assoc as $tb_lapak): ?>
+        <div class="float-start">
+        <table>
+            <thead>
+                <!--  -->
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="ms-3">
+                            <div class="card ms-5 mt-3 mb-5" style="width: 18rem;">
+                                <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h3 class="card-title"> <?= htmlspecialchars($tb_lapak['merk']); ?> </h3>
+                                        <h5 class="card-title"> <?= htmlspecialchars($tb_lapak['sub_merk']); ?> </h5>
+                                        <p class="card-text" style="text-align: center;"> Harga Sewa Perhari</p>
+                                        <p class="card-text" style="text-align: center;"> <?= htmlspecialchars($tb_lapak['harga']); ?> </p>
+                                        <a class="btn btn-primary" href="CRUD/detail.php?id=<?=$tb_lapak['id'];?>" > Detail</a>
+                                    </div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+        <?php endforeach; ?>
     </div>
+    
+<?php include_once 'HPfooter.php'; ?>
 
-    <?php include_once 'HPfooter.php'; ?>
-
-<?php
-
-else :
+<?php else :
     session_unset();
     session_destroy();
-    header("Location: landingpage.php");
+    header("Location: index.php");
     exit();
-
-
 ?>
 <?php endif; ?>
