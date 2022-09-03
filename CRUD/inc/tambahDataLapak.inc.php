95e16ae9b45002a704517d73dff41b3470a6d54e
Arif Laksonodhewo <arifldhewo234@gmail.com>
2022-09-03 21:22:54 +0700

    menambahkan page sistem tambahDataLapak.inc.php menambahkan harga, deskripsi, id_tb_user dan submit_with

--- CRUD/inc/tambahDataLapak.inc.php
+++ CRUD/inc/tambahDataLapak.inc.php
@@ -1,5 +1,10 @@
 <?php
 
+session_start();
+
+$fullnameS = $_SESSION['fullnames'];
+$id_tb_user = $_SESSION['ids'];
+
 require '../../inc/conn.inc.php';
 
 if(isset($_POST['submit'])){
@@ -11,36 +16,40 @@ if(isset($_POST['submit'])){
         }else{
             $sub_merk = $_POST['sub_merk'];
                 if(!preg_match("/^[-a-zA-Z\d\s]*$/", $sub_merk)){
-                    header("Location: gagal.inc.php");
+                    header("Location:../gagalPrompt.php");
                     exit();
                 }else{
                     $tipe_mobil = $_POST['tipe_mobil'];
                         if(!preg_match("/^[a-zA-Z]*$/", $tipe_mobil)){
-                            header("Location: gagal.inc.php");
+                            header("Location:../gagalPrompt.php");
                             exit();
                         }else{
                             $no_polisi = $_POST['no_polisi'];
                                 if(!preg_match("/^[a-zA-Z\d\s]*$/", $no_polisi)){
-                                    header("Location: gagal.inc.php");
+                                    header("Location:../gagalPrompt.php");
                                     exit();
                                 }else{
                                     $warna = $_POST['warna'];
                                         if(!preg_match("/^[a-zA-Z\s]*$/", $warna)){
-                                            header("Location: gagal.inc.php");
+                                            header("Location:../gagalPrompt.php");
                                             exit();
                                         }else{
-                                            $insertData = mysqli_query($conn, 
-                                            "INSERT INTO tb_lapak (merk, sub_merk, tipe_mobil, no_polisi, warna) 
-                                            VALUES ('$merk','$sub_merk','$tipe_mobil','$no_polisi','$warna');
-                                            ");
-
-                                            header("Location: sukses.inc.php");
-                                            exit();
+                                            $harga = $_POST['harga'];
+                                                if(!preg_match("/^[\d]*$/", $harga)){
+                                                    header("Location:../gagalPrompt.php");
+                                                    exit();
+                                                }else{
+                                                    $deskripsi = $_POST['deskripsi'];
+                                                    $insertData = mysqli_query($conn, 
+                                                    "INSERT INTO tb_lapak (id_tb_user, merk, sub_merk, tipe_mobil, no_polisi, warna, harga, submit_with, deskripsi) 
+                                                    VALUES ('$id_tb_user' ,'$merk','$sub_merk','$tipe_mobil','$no_polisi','$warna','$harga','$fullnameS','$deskripsi');
+                                                    ");
+                                                    header("Location: ../suksesPrompt.php");
+                                                    exit();
+                                                }
                                         }
                                 }
                         }
                 }
         }
 }
-
-
