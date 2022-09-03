242e8471bbd41b0808d5645f469b952b4ebeb6d0
Arif Laksonodhewo <arifldhewo234@gmail.com>
2022-09-03 21:09:44 +0700

    menambahkan field harga, deskripsi, dan menghapus id pada select

--- CRUD/tambahDataLapak.php
+++ CRUD/tambahDataLapak.php
@@ -37,7 +37,7 @@
                                     <td>
                                         <div class="ms-5 mt-3">
                                         <label for="" class="form-label"> Jenis Mobil </label>
-                                        <select class="form-select" name="tipe_mobil" id="" required>
+                                        <select class="form-select" name="tipe_mobil" required>
                                             <option value="">--Pilih Tipe Mobil--</option>
                                             <option value="Sedan">Sedan</option>
                                             <option value="Hatchback">Hatchback</option>
@@ -60,7 +60,23 @@
                                         <label for="" class="form-label"> Warna </label>
                                         <input type="text" name="warna" class="form-control" placeholder="Seperti 'Hitam'" required>
                                     </div>
-                                </td> 
+                                </td>
+                                <td>
+                                    <div class="ms-5 mt-3">
+                                        <label for="" class="form-label"> Harga </label>
+                                        <input type="text" name="harga" class="form-control" placeholder="Seperti '1000000'" required>
+                                    </div>
+                                </td>  
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="ms-5 mt-3">
+                                            <div class="form-floating">
+                                                <textarea class="form-control" name="deskripsi" placeholder="Leave a comment here" id="floatingTextarea1" style="width: 250px ; height: 150px" required></textarea>
+                                                <label for="floatingTextarea1">Masukkan Deskripsi Barang</label>
+                                            </div>
+                                        </div>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>
