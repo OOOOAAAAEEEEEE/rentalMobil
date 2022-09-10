e2ca5ebc891cb9ef0886a3911bbe4a5082548e6a
Arif Laksonodhewo <arifldhewo234@gmail.com>
2022-09-10 19:57:36 +0700

    Changing channel id

--- HPheader.php
+++ HPheader.php
@@ -12,11 +12,11 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Homepage</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
-  new Crate({
-    server: '1018013547587973130',
-    channel: '1018013548032565288'
-  })
+  <script src='https://cdn.jsdelivr.net/npm/@widgetbot/crate@3' async defer>
+    new Crate({
+        server: '1018013547587973130', // Testing Server
+        channel: '1018129351662911530' // #rules
+    })
 </script>
 
 </head>
