21c22bc9f1939195fe6365d2b87cc0b5e58d9a25
Arif Laksonodhewo <arifldhewo234@gmail.com>
2022-09-10 18:32:20 +0700

    Menambahkan fitur diskusi yang terintegrasi dengan discord, dan juga menambah keamanan pada dashboard

--- dashboard.php
+++ dashboard.php
@@ -10,23 +10,23 @@
 
     <div class="container">
         <div class="">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div id="carouselMainImg" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="img/uploads/1203181.jpg" class="d-block w-100" alt="...">
+                        <img src="https://via.placeholder.com/1200x400" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="img/uploads/1203335.jpg" class="d-block w-100" alt="...">
+                        <img src="https://via.placeholder.com/1200x500" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="img/uploads/1209038.jpg" class="d-block w-100" alt="...">
+                        <img src="https://via.placeholder.com/1200x600" class="d-block w-100" alt="...">
                     </div>
                 </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselMainImg" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselMainImg" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
@@ -46,12 +46,12 @@
                 <div class="">
                     <div class="col">
                         <div class="card mx-md-3 mx-lg-auto mb-3" style="width: 18.5rem;">
-                            <img src="https://via.placeholder.com/100" class="card-img-top img-thumbnail" alt="...">
+                            <img style="max-width: 300px; max-height: 150px;" src="https://via.placeholder.com/300x150" class="card-img-top img-thumbnail" alt="...">
                                 <div class="card-body shadow-lg bg-body rounded p-4">
                                     <h3 class="card-title"> <?= htmlspecialchars($tb_lapak['merk']); ?> </h3>
                                     <h5 class="card-title"> <?= htmlspecialchars($tb_lapak['sub_merk']); ?> </h5>
                                     <p class="card-text" style="text-align: center;"> Harga Sewa Perhari</p>
-                                    <p class="card-text" style="text-align: center;"> <?= htmlspecialchars($tb_lapak['harga']);?></p>
+                                    <p class="card-text" style="text-align: center;">Rp<?=htmlspecialchars($tb_lapak['harga']);?></p>
                                     <div class="d-grid gap-1">
                                         <a class="btn btn-primary" href="CRUD/detail.php?id=<?=$tb_lapak['id'];?>" > Detail</a>
                                     </div>
