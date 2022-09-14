f6e4b536fa8488486d06d3178491d4b6babe7241
Arif Laksonodhewo <arifldhewo234@gmail.com>
2022-09-14 14:14:32 +0700

    Menambahkan database baru

--- inc/conn.inc.php
+++ inc/conn.inc.php
@@ -1,8 +1,13 @@
 <?php
 
+// $hostname = 'localhost';
+// $usernamee = 'id19565467_mydb';
+// $password = 'KamiKamiKami_123';
+// $dbname = 'id19565467_rentalmobil';
+
 $hostname = 'localhost';
 $usernamee = 'root';
 $password = '';
-$dbname = 'rentalMobil';
+$dbname = 'id19565467_rentalmobil';
 
 $conn = mysqli_connect($hostname,$usernamee,$password,$dbname);
